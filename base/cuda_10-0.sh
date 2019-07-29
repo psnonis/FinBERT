41035bcc29afb4be8dbc8f7ea75add796c0adc32
Pri <43147403+psnonis@users.noreply.github.com>
2019-07-29 16:10:13 -0700

    Update cuda_10-0.sh

--- base/cuda_10-0.sh
+++ base/cuda_10-0.sh
@@ -2,4 +2,4 @@ wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/
 sudo dpkg -i cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
 sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
 sudo apt-get update
-sudo apt-get install cuda
+sudo apt-get install -yq cuda-10-0
