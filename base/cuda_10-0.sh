1bb7a19159dd0f3cbf754ce5f2adf865bbff7db1
Pri <43147403+psnonis@users.noreply.github.com>
2019-07-29 14:56:40 -0700

    Update cuda_10-0.sh

--- base/cuda_10-0.sh
+++ base/cuda_10-0.sh
@@ -1,33 +1,5 @@
-# Add NVIDIA package repositories
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
 sudo dpkg -i cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
 sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
 sudo apt-get update
-wget http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1804/x86_64/nvidia-machine-learning-repo-ubuntu1804_1.0.0-1_amd64.deb
-sudo apt install ./nvidia-machine-learning-repo-ubuntu1804_1.0.0-1_amd64.deb
-sudo apt-get update
-
-# Install NVIDIA driver
-sudo apt-get install --no-install-recommends nvidia-driver-418
-# Reboot. Check that GPUs are visible using the command: nvidia-smi
-
-nvidia-smi
-
-# Install development and runtime libraries (~4GB)
-sudo apt-get install --no-install-recommends \
-     cuda-10-0 \
-     libcudnn7=7.6.0.64-1+cuda10.0  \
-     libcudnn7-dev=7.6.0.64-1+cuda10.0
-
-
-# Install TensorRT. Requires that libcudnn7 is installed above.
-sudo apt-get update
-sudo apt-get install -y --no-install-recommends libnvinfer-dev=5.1.5-1+cuda10.0
-
-#curl -s -L https://nvidia.github.io/nvidia-container-runtime/gpgkey | \
-#sudo apt-key add -
-#distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
-
-#curl -s -L https://nvidia.github.io/nvidia-container-runtime/$distribution/nvidia-container-runtime.list | \
-#sudo tee /etc/apt/sources.list.d/nvidia-container-runtime.list
-#sudo apt-get update
+sudo apt-get install cuda
