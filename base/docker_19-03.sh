7aa345d98a85ba1109e47912412df25e0a3dd7f9
Pri <43147403+psnonis@users.noreply.github.com>
2019-07-29 16:09:33 -0700

    Update docker_19-03.sh

--- base/docker_19-03.sh
+++ base/docker_19-03.sh
@@ -22,5 +22,7 @@ sudo apt-get install -yq --autoremove \
      docker-ce docker-ce-cli containerd.io
 
 docker version
+docker run -it --rm --gpus all ubuntu nvidia-smi
+
 docker login -u '$oauthtoken' --password 'dWcycWEwMzZ1Nm92dmppOHR0dHUzMW9sbzM6Mzk3MWQyZjgtMmRhZi00YTA2LTllMmMtMjExOTljZTYzMGM4' nvcr.io
 docker pull nvcr.io/nvidia/tensorflow:19.06-py3
