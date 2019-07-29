26e75b3a77634167fcd423c39ae79ee1ad32834f
Pri <43147403+psnonis@users.noreply.github.com>
2019-07-29 15:08:24 -0700

    Update init.sh

--- base/init.sh
+++ base/init.sh
@@ -1,5 +1,5 @@
 echo export PATH='${PATH}':.                      >> ~/.bashrc
 echo  alias   ls=ls -lh --group-directories-first >> ~/.bashrc
 
-sudo echo 158.175.88.82   v100x2-fin.finbert.com v100x2-fin >> /etc/hosts
-sudo echo 52.116.175.148  v100x2-pre.finbert.com v100x2-pre >> /etc/hosts
+echo 158.175.88.82   v100x2-fin.finbert.com v100x2-fin >> /etc/hosts
+echo 52.116.175.148  v100x2-pre.finbert.com v100x2-pre >> /etc/hosts
