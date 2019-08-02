6d47264b77f7e8e103963136b5a9db60340e187f
root <root@w251.cloud.lan>
2019-08-02 23:04:05 +0000

    WIP

--- eval/test.sh
+++ eval/test.sh
@@ -1,10 +1,8 @@
 #! /bin/bash
 
-# bert.sh FinBERT-Prime 128 250000 10000 : for 128 MSL and 250K training steps and 10K warmup steps
-
 clear
 
-[[ ! -e ../bert-gpu ]] && echo -e "\nPlease call from [work] folder, exiting...\n" && exit 1
+[[ ! -e ../bert ]] && echo -e "\nPlease call from [eval] folder, exiting...\n" && exit 1
 
 bert=$(realpath ../bert)
 base=$(realpath ../base)
@@ -15,10 +13,6 @@ work=$(realpath  .     )
 
 FIN=${1:-FinBERT-Error}
 MSL=${2:-128}
-NTS=${3:-250000}
-NWS=${4:-10000}
-TBS=${5:-96}
-BWI=${6:-0}
 
 # Pre-Training History
 # 128 x 128 : OOM
@@ -40,26 +34,14 @@ else
 
 fi
 
-GPU=0
 DTS=`date +'%m%d%H%M%S'`
 CFG=${base}/uncased_L-12_H-768_A-12/bert_config.json
 DAT=${data}/????.tfrecord.??-${MSL}-${MPS}
-
-
-
 OUT=${ckpt}/${FIN}
-LOG=${ckpt}/${FIN}/events.out.training.${DTS}.${MSL}x${TBS}.${NTS}.log
-
-if [[ ${BWI} == 1 ]]; then
-# BERT Weights Initialization
-  INI=${base}/uncased_L-12_H-768_A-12/bert_model.ckpt
-# else
-# From Scratch Initialization
-# INI=${OUT}/model.ckpt
-fi
+LOG=${ckpt}/${FIN}/events.out.testing.${DTS}.${MSL}.log
 
 echo
-echo Pre-Training : ${FIN}
+echo Testing : ${FIN}
 echo
 echo CFG : ${CFG}
 echo
@@ -70,35 +52,19 @@ echo
 echo OUT : ${OUT}
 echo
 
-CMD="python3 ${bert}/run_pretraining.py"
+CMD="python3 -W ignore ${bert}/run_pretraining.py"
 
 CMD+=" --bert_config_file=${CFG}"
 CMD+=" --input_file=${DAT}"
 CMD+=" --output_dir=${OUT}"
 
-[[ -n ${INI} ]] && CMD+=" --init_checkpoint=${INI}"
-
 CMD+=" --do_train=False"
 CMD+=" --do_eval=True"
 
-CMD+=" --train_batch_size=${TBS}"
 CMD+=" --max_seq_length=${MSL}"
 CMD+=" --max_predictions_per_seq=${MPS}"
 
-CMD+=" --num_train_steps=${NTS}"
-CMD+=" --num_warmup_steps=${NWS}"
-
-CMD+=" --save_checkpoints_steps=5000"
-
-CMD+=" --learning_rate=1e-4"
-CMD+=" --use_fp16"
-CMD+=" --use_xla"
 CMD+=" --report_loss"
-CMD+=" --horovod"
-
-if [ $GPU -gt 1 ] ; then
-  CMD="mpiexec --allow-run-as-root -np ${GPU} --bind-to socket ${CMD}"
-fi
 
 echo LOG : ${LOG}
 echo
